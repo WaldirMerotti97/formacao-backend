5cc401913b85ff65a93dbf46db8d228547c444a1
waldmer <waldir_merotti@hotmail.com>
2023-09-25 19:22:30 -0300

    feat: add endpoint find by id

---
+++ user-service-api/src/main/java/br/com/waldir/userserviceapi/controller/impl/UserControllerImpl.java
@@ -0,0 +1,19 @@
+package br.com.waldir.userserviceapi.controller.impl;
+
+import br.com.waldir.userserviceapi.controller.UserController;
+import br.com.waldir.userserviceapi.entity.User;
+import br.com.waldir.userserviceapi.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserControllerImpl implements UserController {
+
+    private final UserService userService;
+    @Override
+    public ResponseEntity<User> findById(String id) {
+        return ResponseEntity.ok().body(userService.findById(id));
+    }
+}
---
+++ user-service-api/src/main/java/br/com/waldir/userserviceapi/service/UserService.java
@@ -0,0 +1,17 @@
+package br.com.waldir.userserviceapi.service;
+
+import br.com.waldir.userserviceapi.entity.User;
+import br.com.waldir.userserviceapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User findById(final String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
