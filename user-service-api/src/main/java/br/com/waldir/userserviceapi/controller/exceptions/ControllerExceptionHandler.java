ccbd73dc4210172e2a0494248ff392b748b3857e
waldmer <waldir_merotti@hotmail.com>
2023-09-29 19:30:52 -0300

    feat: add validation to email if already exists

--- user-service-api/src/main/java/br/com/waldir/userserviceapi/controller/exceptions/ControllerExceptionHandler.java
+++ user-service-api/src/main/java/br/com/waldir/userserviceapi/controller/exceptions/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package br.com.waldir.userserviceapi.controller.exceptions;
 import jakarta.servlet.http.HttpServletRequest;
 import models.exceptions.ResourceNotFoundException;
 import models.exceptions.ValidationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -50,4 +51,17 @@ public class ControllerExceptionHandler {
 
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    ResponseEntity<StandardError> handleDataIntegrityViolationException(final DataIntegrityViolationException ex, final HttpServletRequest request) {
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                StandardError.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.CONFLICT.value())
+                        .error(HttpStatus.CONFLICT.getReasonPhrase())
+                        .message(ex.getMessage())
+                        .path(request.getRequestURI())
+                        .build()
+        );
+    }
 }
--- user-service-api/src/main/java/br/com/waldir/userserviceapi/repository/UserRepository.java
+++ user-service-api/src/main/java/br/com/waldir/userserviceapi/repository/UserRepository.java
@@ -4,6 +4,11 @@ import br.com.waldir.userserviceapi.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(final String email);
+
 }
