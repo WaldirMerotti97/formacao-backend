e27171253a5eb44805bb7c9841b139f9b5229b92
waldmer <waldir_merotti@hotmail.com>
2023-09-30 10:51:45 -0300

    test: add test to find by id with invalid id, failing the test

--- user-service-api/src/test/java/br/com/waldir/userserviceapi/service/UserServiceTest.java
+++ user-service-api/src/test/java/br/com/waldir/userserviceapi/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package br.com.waldir.userserviceapi.service;
 import br.com.waldir.userserviceapi.entity.User;
 import br.com.waldir.userserviceapi.mapper.UserMapper;
 import br.com.waldir.userserviceapi.repository.UserRepository;
+import models.exceptions.ResourceNotFoundException;
 import models.responses.UserResponse;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -13,8 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -45,6 +45,23 @@ class UserServiceTest {
         assertNotNull(response);
         assertEquals(UserResponse.class, response.getClass());
         verify(repository, times(1)).findById(anyString());
+        verify(mapper, times(1)).fromEntity(any(User.class));
     }
+
+    @Test
+    void whenCallFindByIdWithInvalidIdThenThrowResourceNotFoundException(){
+
+        when(repository.findById(anyString())).thenReturn(Optional.empty());
+
+        var exception = assertThrows(ResourceNotFoundException.class,
+                () -> service.findById("1")
+        );
+
+        verify(repository, times(1)).findById(anyString());
+        verify(mapper, times(0)).fromEntity(any(User.class));
+        assertEquals("Object not found. Id: 1, Type UserResponse", exception.getMessage());
+    }
+
+
 
 }
