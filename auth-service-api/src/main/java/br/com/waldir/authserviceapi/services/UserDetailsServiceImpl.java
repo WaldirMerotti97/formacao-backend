85d9ce4c62ff414e04dc046ff5aae05b896deb28
waldmer <waldir_merotti@hotmail.com>
2023-10-11 21:55:46 -0300

    feat: add authentication impl

---
+++ auth-service-api/src/main/java/br/com/waldir/authserviceapi/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package br.com.waldir.authserviceapi.services;
+
+import br.com.waldir.authserviceapi.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        final var entity = repository.findByEmail(email)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("User not found: " + email)
+                );
+
+        return null;
+    }
+}
